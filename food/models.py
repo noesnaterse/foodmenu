15142b90ca03143e66c6129269a6d5953078b4e3
Noë Snaterse <noe@wiredpea.com>
2024-05-07 18:11:29 +0200

    Finishing the templates

--- food/models.py
+++ food/models.py
@@ -5,6 +5,10 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField()
+    image = models.CharField(
+        max_length=500,
+        default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg",
+    )
 
     def __str__(self):
         return self.name
